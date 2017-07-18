233c0611e971058c082eb064e9b67c951a882677
KZafirov <kiril_zafirov@yahoo.com>
2017-07-18 12:50:16 +0200

    Add project files.

---
+++ IdentityUsersRoles/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IdentityUsersRoles.MvcApplication" Language="C#" %>
---
+++ IdentityUsersRoles/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdentityUsersRoles.Startup))]
+namespace IdentityUsersRoles
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
